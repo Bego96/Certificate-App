e3d88065ebe184bf387c938463e867bca4a867ee
Bego96 <begic.ismar96@gmail.com>
2023-09-30 13:54:17 +0200

    Certificate overview front end completed

--- src/components/certificateOverview.jsx
+++ src/components/certificateOverview.jsx
@@ -1,37 +1,328 @@
 const CertificateOverview = () => {
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>
-                        Supplier
-                    </th>
-                    <th>
-                        Certificate type
-                    </th>
-                    <th>
-                        Valid from
-                    </th>
-                    <th>
-                        Valid to
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        ANDEMS 
-                    </td>
-                    <td>
-                        CCC certificate
-                    </td>
-                    <td>
-                        15.08.2017.
-                    </td>
-                    <td>
-                        04.04.2024.
-                    </td>
-                </tr>
-            </table>
-        </div>
+        
+<div class="w-full">
+	<button type="button" className="py-2 px-4 bg-green-400 text-white mb-2">New certificate</button>
+	<table class="text-slate-600 text-left w-full">
+		<thead class=" flex w-full border-[1px]">
+			<tr class="flex w-full ">
+				<th class="p-4 w-1/4">Supplier</th>
+				<th class="p-4 w-1/4">Certificate type</th>
+				<th class="p-4 w-1/4">Valid from</th>
+				<th class="p-4 w-1/4">Valid to</th>
+			</tr>
+		</thead>
+   
+		<tbody class=" flex flex-col items-center justify-between overflow-y-auto w-full h-[50vh]" style={{ overflowY: 'overlay' }}>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+			<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+          	<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+          	<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+          	<tr class="flex w-full mb-4">
+				<td class="p-4 w-1/4">Dogs</td>
+				<td class="p-4 w-1/4">Cats</td>
+				<td class="p-4 w-1/4">Birds</td>
+				<td class="p-4 w-1/4">Fish</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
     )
 }
 
--- src/components/mainContainer.jsx
+++ src/components/mainContainer.jsx
@@ -5,7 +5,7 @@ import CertificateOverview from "./certificateOverview";
 
 const MainContainer = (props) => {
     return (
-        <div className={`laptop:absolute laptop:top-16 box-border p-10 ${props.showSidebar ? ' laptop:left-[30%] desktop:left-[20%] laptop:w-[70%] desktop:w-[80%]' : ' laptop:left-0 w-full'}`}>
+        <div className={` laptop:absolute laptop:top-16 box-border p-10 ${props.showSidebar ? ' laptop:left-[30%] desktop:left-[20%] laptop:w-[70%] desktop:w-[80%]' : ' laptop:left-0 w-full'}`}>
             <Routes>
                 <Route path='/' element={<Homepage />}/>
                 <Route path='/Certificate-overview' element={<CertificateOverview />}/>
